598f0818d0bfe6a21f918c01ea4e5365808f1599
tamb <t.saporito@gmail.com>
2019-08-03 14:00:39 -0400

    repack webpack

--- example/dist/src.a2b27638.js
+++ example/dist/src.a2b27638.js
@@ -1049,7 +1049,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33511" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40117" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
