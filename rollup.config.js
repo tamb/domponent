698233f65a0def72fa0d2fbeeb8618ad6fc2edec
tamb <t.saporito@gmail.com>
2019-08-19 20:06:53 -0400

    npm install stip code

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import { terser } from "rollup-plugin-terser";
+import stripCode from 'rollup-plugin-strip-code';
 
 export default [
   {
     input: "src/index.js",
     output: {
-      file: "dist/domponent.umd.js",
+      file: "dist/domponent.production.min.js",
       format: "umd",
       name: "Domponent"
     },
@@ -15,7 +16,19 @@ export default [
         mangle: {
           keep_classnames: true
         }
+      }),
+      stripCode({
+        start_comment: "START.DEV",
+        end_comment: "END.DEV"
       })
     ]
-  }
+  },
+  {
+    input: "src/index.js",
+    output: {
+      file: "dist/domponent.development.js",
+      format: "umd",
+      name: "Domponent"
+    }
+  },
 ];
