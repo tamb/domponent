357f866f0eaabeb5577037718503038e517e34a9
tamb <t.saporito@gmail.com>
2019-08-20 21:28:53 -0400

    update some development code and create an example.  Example is WIP

---
+++ examples/dev-build-errors/src/index.js
@@ -0,0 +1,56 @@
+import { Init } from 'domponent/dist/domponent.development'
+import Counter from "./Counter";
+import CurrentTime from "./CurrentTime";
+import DisplayAnything from "./DisplayAnything";
+import Name from "./Name";
+import domInsert from "./domInsert";
+
+console.time('appCreation');
+const App = new Init({
+  selector: document.getElementById("root"),
+  components: {
+    Counter,
+    CurrentTime,
+    DisplayAnything,
+    Name
+  },
+  appCreated: () => console.log("app created")
+});
+console.timeEnd('appCreation');
+
+// const mills = document.getElementById('mills');
+// const up = document.getElementById('up');
+// const down = document.getElementById('down');
+// const total = document.getElementById('total');
+
+// const max = 15;
+
+// let totalCount = 0;
+// let grow = 1;
+// let shrink = 60;
+// setInterval(()=>{
+//   --shrink;
+//   mills.textContent = ++grow;
+//   if(grow === max){
+//     grow = 0;
+//     total.textContent = ++totalCount;
+//   }
+//   if(shrink === 0){
+//     shrink = max;
+//   }
+//   up.style.height = grow * 6+'px';
+//   down.style.height = shrink * 6+'px';
+
+// },1);
+
+window.DomponentApp = App;
+
+setTimeout(() => {
+  domInsert("id2");
+  App.createComponent(document.getElementById("id2"));
+}, 1000);
+
+setTimeout(() => {
+  domInsert("id3");
+  App.createComponent(document.getElementById("id3"));
+}, 3000);
