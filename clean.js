26b6204c03da583f516004b20c298ecc42a24f22
Tom Saporito <tsaporito@fastmail.com>
2019-08-23 15:08:19 -0400

    beta commit

--- clean.js
+++ clean.js
@@ -3,6 +3,6 @@ const rimraf = require('rimraf');
 console.log('beginning clean');
 rimraf('*.tgz', {}, ()=>console.log('removed pack'));
 rimraf('/dist', {}, ()=>console.log('removed rollup dist'));
-rimraf('examples/html/dist', {}, ()=>console.log('removed example dist'));
+rimraf('examples/html-*/dist', {}, ()=>console.log('removed example dist'));
 rimraf('examples/**/node_modules', {}, ()=>console.log('removed example node_modules'));
 rimraf('examples/**/package-lock.json', {}, ()=>console.log('removed example package-lock.json'));
---
+++ examples/html-prod/src/DisplayAnything.js
@@ -0,0 +1,20 @@
+import { Exponent } from 'domponent';
+
+export default class DisplayAnything extends Exponent {
+  constructor(el) {
+    super(el);
+    this.code = this.$root.querySelector(".code");
+    this.objects = this.$root.querySelector(".propObjects");
+    this.displayProps();
+  }
+
+  propsDidUpdate(oldProps) {
+    if (oldProps.goBold !== this.props.goBold) {
+      this.displayProps();
+    }
+  }
+  displayProps() {
+    this.code.textContent = JSON.stringify(this.props, undefined, 4);
+    this.objects.textContent = JSON.stringify(this.$p.goBold.parentComponentKey, undefined, 4);
+  }
+}
