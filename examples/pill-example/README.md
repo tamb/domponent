c09a30d348d55b70b3ffd5289c7344b849742601
tamb <t.saporito@gmail.com>
2019-08-24 22:21:36 -0400

    updatingprod example

---
+++ examples/pill-example/README.md
@@ -0,0 +1,3 @@
+`npm install -g http-server`
+`http-server`
+`localhost:8080`
---
+++ examples/pill-example/webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    entry: {
+      main:  './src/index.js',
+      counter: './src/counter.js'
+    }
+
+};
