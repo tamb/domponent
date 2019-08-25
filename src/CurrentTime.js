38e2c1c044d5865f892268f87ae765f9302f9d36
tamb <t.saporito@gmail.com>
2019-08-24 21:59:47 -0400

    Updates

---
+++ src/CurrentTime.js
@@ -0,0 +1,25 @@
+import { Component } from 'domponent';
+
+export default class CurrentTime extends Component {
+  constructor(el) {
+    super(el);
+    this.state = {
+      hours: "",
+      seconds: "",
+      minutes: ""
+    };
+
+    setInterval(() => {
+      this.changeTime();
+    }, 1000);
+  }
+
+  changeTime() {
+    const date = new Date();
+    this.setState({
+      seconds: date.getSeconds(),
+      hours: date.getHours(),
+      minutes: date.getMinutes()
+    });
+  }
+}
