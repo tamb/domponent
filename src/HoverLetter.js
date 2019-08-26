fbf43c38376dc59c1023e0b9e3355b691aab3477
tamb <t.saporito@gmail.com>
2019-08-25 21:44:07 -0400

    Updates

--- src/HoverLetter.js
+++ src/HoverLetter.js
@@ -1,35 +1,43 @@
-import { Exponent } from 'domponent';
+import { Exponent } from "domponent";
 
-export default class HoverLetter extends Exponent{
-    constructor(conf){
-        super(conf);
-        this.letters = this.$root.querySelector('[data-ref="letters"]');
-        this.splitWord();
+export default class HoverLetter extends Exponent {
+  constructor(conf) {
+    super(conf);
+    this.letters = this.$root.querySelector('[data-ref="letters"]');
+    this.styleID = "hover-letter__style";
+    this.splitWord();
+  }
+
+  splitWord() {
+    let html = this.letters.innerText
+      .toString()
+      .split("")
+      .map(item => {
+        return `<span tabindex="0" class="hoverable">${item}</span>`;
+      });
+
+    let htmlString = "";
+    html.forEach(item => (htmlString += item));
+    this.letters.innerHTML = "";
+    this.letters.insertAdjacentHTML("afterbegin", htmlString);
+
+    if (!document.getElementById(this.styleID)) {
+      this.placeStyles();
     }
+  }
 
-    splitWord(){
-        let html = this.letters.innerText.toString().split('').map(item => {
-            return `<span tabindex="0" class="hoverable">${item}</span>`;
-        });;
-        
-        
-        let htmlString = '';
-        html.forEach(item => htmlString += item);
-        this.letters.innerHTML = '';
-        this.letters.insertAdjacentHTML('afterbegin', htmlString);
-
-        const style = `<style>
-            .hoverable{
-                transition: transform .25s;
-                display: inline-block;
-                cursor: pointer;
-            }
-            .hoverable:hover,
-            .hoverable:focus{
-                transform: scale(1.4);
-            }
-        </style>`
-        document.head.insertAdjacentHTML('beforeend', style);
-    }
-
+  placeStyles() {
+    const style = `<style id="${this.styleID}">
+        .hoverable{
+            transition: transform .25s;
+            display: inline-block;
+            cursor: pointer;
+        }
+        .hoverable:hover,
+        .hoverable:focus{
+            transform: scale(1.4);
+        }
+    </style>`;
+    document.head.insertAdjacentHTML("beforeend", style);
+  }
 }
