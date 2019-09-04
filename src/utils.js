96f6564038038357dc5e44cfcc7522be17119289
tamb <t.saporito@gmail.com>
2019-09-03 21:10:07 -0400

    rebase to master

--- src/utils.js
+++ src/utils.js
@@ -18,38 +18,56 @@ export function hasCallback(cb) {
 
 // string parsing
 export function splitKeyValuePairs(string) {
-  return string.trim().split(relationalStringEnum.KEY_VALUE);
+  return string
+    .trim()
+    .split(relationalStringEnum.KEY_VALUE)
+    .map(item => item.trim());
 }
 export function splitMultipleValues(string) {
-  return string.trim().split(relationalStringEnum.MULTIPLE_VALUES);
+  return string
+    .trim()
+    .split(relationalStringEnum.MULTIPLE_VALUES)
+    .map(item => item.trim());
 }
 export function splitPropsPassedIn(string) {
-      /* START.DEV */
-      if(!string.includes('<-')){
-        console.error(`🤓 -- "You have bad syntax on this data- value: ${string}.  
+  /* START.DEV */
+  if (!string.includes("<-")) {
+    console.error(`🤓 -- "You have bad syntax on this data- value: ${string}.  
         You are not inheriting props correctly.  It should look like this 'myProp<-MyComponent.myStateField'`);
-      }
-      /* END.DEV */
-  return string.trim().split(relationalStringEnum.INHERITS_FROM);
+  }
+  /* END.DEV */
+  return string
+    .trim()
+    .split(relationalStringEnum.INHERITS_FROM)
+    .map(item => item.trim());
 }
 export function splitMethodCalls(string) {
-    /* START.DEV */
-    if(!string.includes('->')){
-      console.error(`🤓 -- "You have bad syntax on this data- value: ${string}.  
+  /* START.DEV */
+  if (!string.includes("->")) {
+    console.error(`🤓 -- "You have bad syntax on this data- value: ${string}.  
       You are missing an arrow in your method call.  It should look like this 'DOMEvent->MyComponent.myMethod'`);
-    }
-    /* END.DEV */
-  return string.trim().split(relationalStringEnum.METHOD_CALL);
+  }
+  /* END.DEV */
+  return string
+    .trim()
+    .split(relationalStringEnum.METHOD_CALL)
+    .map(item => item.trim());
 }
 export function splitFromComponent(string) {
   /* START.DEV */
-  if(!string.includes('.')){
+  if (!string.includes(".")) {
     console.error(`🤓 -- "You have bad syntax on this data- value: ${string}.  
     You need to have a period (.) like 'MyComponent.myField'`);
   }
   /* END.DEV */
-  return string.trim().split(relationalStringEnum.FROM_COMPONENT);
+  return string
+    .trim()
+    .split(relationalStringEnum.FROM_COMPONENT)
+    .map(item => item.trim());
 }
-export function splitList(string){
-  return string.trim().split(relationalStringEnum.LIST);
+export function splitList(string) {
+  return string
+    .trim()
+    .split(relationalStringEnum.LIST)
+    .map(item => item.trim());
 }
