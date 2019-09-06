2850857f1dd0a1262a958f9d6fd4843c0c70f6b7
tamb <t.saporito@gmail.com>
2019-09-06 16:37:36 -0400

    Updates

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import { Init } from 'domponent'
+import { Init } from "domponent";
 import Counter from "./Counter/Counter";
 import Timer from "./Timer/Timer";
 import DisplayAnything from "./DisplayAnything/DisplayAnything";
 import FavoriteShow from "./FavoriteShow/FavoriteShow";
-import HoverLetter from './HoverLetter/HoverLetter';
-import Navigation from './Navigation/Navigation';
-import ShowCode from './ShowCode/ShowCode';
+import HoverLetter from "./HoverLetter/HoverLetter";
+import Navigation from "./Navigation/Navigation";
+import ShowCode from "./ShowCode/ShowCode";
+import Tabs from "./Tabs/Tabs";
 import domInsert from "./domInsert";
 
-import './app.scss';
+import "./app.scss";
 
 feather.replace();
 
-console.time('appCreation');
+console.time("appCreation");
 const App = new Init({
   selector: document.getElementById("root"),
   components: {
@@ -23,15 +24,18 @@ const App = new Init({
     HoverLetter,
     Navigation,
     ShowCode,
+    Tabs
   },
   appCreated: () => console.log("app created")
 });
-console.timeEnd('appCreation');
+console.timeEnd("appCreation");
 
 window.DomponentApp = App;
 
 setTimeout(() => {
   domInsert("id2");
-  App.createComponent(document.getElementById("id2"), ()=>feather.replace());
-  App.createComponent(document.getElementById("async-props"), ()=>feather.replace());
+  App.createComponent(document.getElementById("id2"), () => feather.replace());
+  App.createComponent(document.getElementById("async-props"), () =>
+    feather.replace()
+  );
 }, 1000);
