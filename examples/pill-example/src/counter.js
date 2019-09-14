8c069da77d4f3501f0903de6150708d2afc20960
tamb <t.saporito@gmail.com>
2019-09-13 20:11:08 -0400

    remove pill example

--- examples/pill-example/src/counter.js
+++
@@ -1,7 +0,0 @@
-console.log('counter loaded');
-window.count = window.count || 0;
-window.count++;
-window.tom = window.tom || {};
-window.tom[`_${window.count}`] = 'new entry';
-
-console.log(window.tom)
