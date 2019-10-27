c5d696bec7c4afc43005161a2fab1e02db8284c3
tamb <t.saporito@gmail.com>
2019-10-27 12:23:43 -0400

    more styles for site

--- examples/site/src/index.js
+++ examples/site/src/index.js
@@ -1,3 +1,7 @@
+import "normalize-scss/fork-versions/default/_normalize.scss";
 import "./fonts.scss";
+import "./base.scss";
 import "./includes/nav/nav";
-import "./include/tags/tags";
+import "./includes/tags/tags";
+import "./includes/header/header";
+import "./includes/btn/btn";
