2603c2c72548b81df1cd7c1d6a7bd4d868762c1c
tamb <t.saporito@gmail.com>
2019-10-28 21:32:27 -0400

    update btn styles

--- examples/site/src/index.js
+++ examples/site/src/index.js
@@ -6,7 +6,37 @@ import "./includes/tags/tags";
 import "./includes/header/header";
 import "./includes/btn/btn";
 import "./includes/markup/markup";
+import {
+  Init,
+  Component
+} from "domponent/dist/domponent.es5.production.min.js";
 
 import feather from "feather-icons";
 
 feather.replace();
+
+class Counter extends Component {
+  constructor(el) {
+    super(el);
+    this.state = {
+      count: 0
+    };
+  }
+
+  increment() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  decrement() {
+    this.setState({ count: this.state.count - 1 });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const app = new Init({
+    selector: document.querySelector(".counter-preview"),
+    components: {
+      Counter
+    }
+  });
+});
