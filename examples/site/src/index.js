e6872337f0a106f8f0b07ea22be6581b54b56619
tamb <t.saporito@gmail.com>
2019-11-12 21:05:22 -0500

    adds jump

--- examples/site/src/index.js
+++ examples/site/src/index.js
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function() {
       e.preventDefault;
       const anchor = e.target;
       console.log(anchor);
-      jump(anchor.href);
+      jump(anchor.getAttribute("href"));
     });
   });
 
