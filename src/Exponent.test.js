1f6c035859b9ee1d7f1fc9018ae357052687158a
Tom Saporito <tamb89@protonmail.com>
2020-05-02 17:35:32 -0400

    adds  tests

--- src/Exponent.test.js
+++ src/Exponent.test.js
@@ -1,21 +1,34 @@
 import Exponent from "./Exponent";
-import { relationalStringEnum } from "./enums";
+import { relationalStringEnum, datasetEnum } from "./enums";
+
+import { toHaveAttribute } from "@testing-library/jest-dom/matchers";
+
+expect.extend({ toHaveAttribute });
 
 describe("Tests Exponent class", () => {
   const componentName = "HelloWorld";
   const id = "mything";
   const key = "myKey";
 
-  document.body.innerHTML = `
+  const image = `<img data-ref="${componentName}.img"/>`;
+
+  const html = `
         <div id="${id}" data-component="${componentName}">
             <h1>Hello from component</h1>
+            ${image}
+
+            <i data-ref-array="${componentName}.icons"></i>;
+            <i data-ref-array="${componentName}.icons"></i>;
+            <i data-ref-array="${componentName}.icons"></i>;
+            <i data-ref-array="${componentName}.icons"></i>;
+
         </div>
     `;
 
+  document.body.innerHTML = html;
+
   const app = {
-    $datasets: {
-      component: "component"
-    },
+    $datasets: datasetEnum,
     $syntax: relationalStringEnum
   };
 
@@ -41,5 +54,27 @@ describe("Tests Exponent class", () => {
 
   test("Exponent is created with proper $name from DOM", () => {
     expect(MyExponent.$name).toBe(componentName);
+  });
+
+  test("Exponent holds $refs object", () => {
+    expect(typeof MyExponent.$refs).toBe("object");
+  });
+
+  test("Exponent $refs to have img", () => {
+    expect(MyExponent.$refs.img).toBeDefined();
+    expect(MyExponent.$refs).toHaveProperty("img");
+    expect(MyExponent.$refs.img).toHaveAttribute(
+      "data-ref",
+      `${componentName}.img`
+    );
+  });
+
+  test("Exponent $refs to have icons", () => {
+    expect(MyExponent.$refs.icons).toBeDefined();
+    expect(MyExponent.$refs).toHaveProperty("icons");
+    expect(MyExponent.$refs.icons[0]).toHaveAttribute(
+      "data-ref-array",
+      `${componentName}.icons`
+    );
   });
 });
